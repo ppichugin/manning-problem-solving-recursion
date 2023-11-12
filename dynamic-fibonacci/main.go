7bd09161526956930c4ea1a4f40b87340097512c
Pyotr Pichugin <p.pichugin@andersenlab.com>
2023-11-12 15:08:57 +0500

    impl dynamic fibonacci

---
+++ dynamic-fibonacci/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var fibonacciValues []int64
+
+func main() {
+	for {
+		// Get n as a string.
+		var nString string
+		fmt.Printf("N: ")
+		fmt.Scanln(&nString)
+
+		// If the n string is blank, break out of the loop.
+		if len(nString) == 0 {
+			break
+		}
+
+		// Convert to int and calculate the Fibonacci number.
+		n, _ := strconv.ParseInt(nString, 10, 64)
+		fmt.Printf("fibonacciOnTheFly(%d) = %d\n", n, fibonacciOnTheFly(n))
+		//fmt.Printf("fibonacciPrefilled(%d)  = %d\n", n, fibonacciPrefilled(n))
+		fmt.Printf("fibonacciBottomUp(%d)  = %d\n", n, fibonacciBottomUp(n))
+	}
+	// Print out all memoized values just so we can see them.
+	for i := 0; i < len(fibonacciValues); i++ {
+		fmt.Printf("%d: %d\n", i, fibonacciValues[i])
+	}
+}
+
+func fibonacciOnTheFly(n int64) int64 {
+	if fibonacciValues == nil {
+		fibonacciValues = make([]int64, 2)
+		fibonacciValues[0] = 0
+		fibonacciValues[1] = 1
+	}
+
+	if n < int64(len(fibonacciValues)) {
+		return fibonacciValues[n]
+	}
+
+	for i := int64(len(fibonacciValues)); i <= n; i++ {
+		fibonacciValues = append(fibonacciValues, fibonacciValues[i-1]+fibonacciValues[i-2])
+	}
+	return fibonacciValues[n]
+}
+
+func fibonacciPrefilled(n int64) int64 {
+	initializeSlice()
+	return fibonacciValues[n]
+}
+
+// Allocate the slice and give it 93 entries.
+func initializeSlice() {
+	fibonacciValues = make([]int64, 93)
+	fibonacciValues[0] = 0
+	fibonacciValues[1] = 1
+	for i := 2; i < len(fibonacciValues); i++ {
+		fibonacciValues[i] = fibonacciValues[i-1] + fibonacciValues[i-2]
+	}
+}
+
+func fibonacciBottomUp(n int64) int64 {
+	if n <= 1 {
+		return n
+	}
+
+	var fibI, fibIMinus1, fibIMinus2 int64
+	fibIMinus2 = 0
+	fibIMinus1 = 1
+	fibI = fibIMinus1 + fibIMinus2
+	for i := int64(1); i < n; i++ {
+		// Calculate this value of fibI.
+		fibI = fibIMinus1 + fibIMinus2
+
+		// Set fibIMinus2 and fibIMinus1 for the next value.
+		fibIMinus2 = fibIMinus1
+		fibIMinus1 = fibI
+	}
+	return fibI
+}
